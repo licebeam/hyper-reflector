b668f0ba5ae21a274612d6ed370e9bbd76b629d6
aburd <aaron.burdick@protonmail.com>
2025-02-05 23:46:35 +0900

    redo config

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,16 @@
 import { app } from 'electron';
 const path = require('path');
 const fs = require("fs");
+// import path from "path";
+// import fs from "fs";
 
 export type AppConfig = {
-  emuPath: string;
   filePathBase: string;
   isDev: boolean;
 }
 
 export type EmulatorConfig = {
+  emuPath: string;
   fightcadePath: string;
   luaPath: string;
 }
@@ -18,19 +20,57 @@ export type Config = {
   emulator: EmulatorConfig;
 }
 
-function getConfigFileMap(filePathBase: string): Record<string, string> {
-  const filePath = path.join(filePathBase, 'config.txt');
-  const s: string = fs.readFileSync(filePath, { encoding: 'utf8' });
-
-  // We split all lines then, all become key-values
-  // pairs
-  return s.split('\n')
-    .reduce((kv, line) => {
-      const [k, v] = line.split('=')
-      return { ...kv, [k]: v }
-    }, {});
+const DEFAULT_CONFIG: EmulatorConfig = {
+  emuPath: "",
+  fightcadePath: "",
+  luaPath: "",
 }
 
+function getConfigFilePath(filePathBase: string) {
+  return path.join(filePathBase, "..", "resources/config.json")
+}
+
+function isEmulatorConfig(config: unknown): config is EmulatorConfig {
+  return typeof config === 'object' && 
+    'emuPath' in config && 
+    'fightcadePath' in config && 
+    'luaPath' in config;
+}
+
+function getEmulatorConfig({filePathBase}: AppConfig): EmulatorConfig {
+  const filePath = getConfigFilePath(filePathBase);
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, JSON.stringify(DEFAULT_CONFIG, null, 2), {})
+  }
+  const config = JSON.parse(fs.readFileSync(filePath, { encoding: 'utf8' }));
+  if (!isEmulatorConfig(config)) {
+    console.warn('emulator config file was corrupted, writing a new one');
+    fs.writeFileSync(filePath, JSON.stringify(DEFAULT_CONFIG, null, 2), {})
+  }
+
+  return config;
+}
+
+export async function setEmulatorConfig({filePathBase}: AppConfig, emuPath: string) {
+  // this will throw if the path is invalid
+  path.parse(emuPath)
+
+  // TODO: maybe set config individually?
+  // here we just write all the defaults from emupath
+  const emulatorConfig = {
+    emuPath,
+    fightcadePath: path.join(emuPath, "fcadefbneo.exe"),
+    luaPath: path.join(filePathBase, 'lua', '3rd_training_lua', '3rd_training.lua'),
+  }
+
+  fs.writeFileSync(
+    getConfigFilePath(filePathBase), 
+    JSON.stringify(emulatorConfig, null, 2),
+  );
+}
+
+// TODO: maybe this should just be some context we carry
+// around the app
 function getAppConfig(): AppConfig {
   const isDev = !app.isPackaged;
 
@@ -39,38 +79,15 @@ function getAppConfig(): AppConfig {
     ? path.join(app.getAppPath(), "src")
     : process.resourcesPath
 
-  const configFileMap = getConfigFileMap(filePathBase);
-
   return {
-    emuPath: configFileMap.emuPath,
     filePathBase,
     isDev,
   }
 }
 
-function getEmulatorConfig({ emuPath, filePathBase }: AppConfig) {
-  return {
-    fightcadePath: path.join(emuPath, "fcadefbneo.exe"),
-    luaPath: path.join(filePathBase, 'lua', '3rd_training_lua', '3rd_training.lua'),
-  }
-}
-
 export function getConfig() {
-  let app: AppConfig;
-  try {
-    app = getAppConfig();
-  } catch(e) {
-    console.error(`could not get app config: ${e}`)
-    throw e;
-  }
-
-  let emulator: EmulatorConfig;
-  try {
-    emulator = getEmulatorConfig(app);
-  } catch(e) {
-    console.error(`could not get emulator config: ${e}`)
-    throw e;
-  }
+  const app = getAppConfig();
+  const emulator = getEmulatorConfig(app);
 
   return {
     app,
