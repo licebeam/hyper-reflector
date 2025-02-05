27841ad2d40ed53ae70a705f702f8529d62362e1
aburd <aaron.burdick@protonmail.com>
2025-02-05 23:50:31 +0900

    feat: add new json file

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    extraResource: ['./src/lua', './src/fbneo_commands.txt', './src/reflector_commands.txt', './src/config.txt']
+    extraResource: ['./src/lua', './src/fbneo_commands.txt', './src/reflector_commands.txt', './src/config.txt', './resources/config.json']
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
