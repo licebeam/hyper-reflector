f28185c25a58fe65b0593681b9d95e91733967e5
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-10 10:01:31 +0900

    cleans up a few files with prettier

--- src/main.ts
+++ src/main.ts
@@ -1,236 +1,241 @@
-import { app, BrowserWindow, ipcMain, dialog, Notification } from 'electron';
-import started from 'electron-squirrel-startup';
-import { sendCommand, readCommand, readStatFile } from './sendHyperCommands';
-import { startPlayingOnline, startSoloMode } from './loadFbNeo';
-import { getConfig, type Config } from './config';
+import { app, BrowserWindow, ipcMain, dialog, Notification } from 'electron'
+import started from 'electron-squirrel-startup'
+import { sendCommand, readCommand, readStatFile } from './sendHyperCommands'
+import { startPlayingOnline, startSoloMode } from './loadFbNeo'
+import { getConfig, type Config } from './config'
 // external api
 import api from './external-api/requests'
 
 // - FIREBASE AUTH CODE - easy peasy
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { firebaseConfig } from './private/firebase';
+import { initializeApp } from 'firebase/app'
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { firebaseConfig } from './private/firebase'
 
 // Initialize Firebase
-const fbapp = initializeApp(firebaseConfig);
+const fbapp = initializeApp(firebaseConfig)
 
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(fbapp);
+const auth = getAuth(fbapp)
 // END FIREBASE
 
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
-const isDev = !app.isPackaged;
+const isDev = !app.isPackaged
 
-let filePathBase = process.resourcesPath;
+let filePathBase = process.resourcesPath
 //handle dev mode toggle for file paths.
 if (isDev) {
-  filePathBase = path.join(app.getAppPath(), "src");
+    filePathBase = path.join(app.getAppPath(), 'src')
 }
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
-  app.quit();
+    app.quit()
 }
 
 const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      // potential security issue = https://stackoverflow.com/questions/48148021/how-to-import-ipcrenderer-in-react
-      // but this allows us to use ipc calls in useEffects in react
-      // nodeIntegration: true, 
-      // contextIsolation: false,
-      // 
-      preload: path.join(__dirname, 'preload.js'),
-    },
-    autoHideMenuBar: true,
-  });
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        },
+        autoHideMenuBar: true,
+    })
 
-  let config: Config
-  try {
-    config = getConfig();
-    console.log({ config })
-  } catch (error) {
-    mainWindow.webContents.send('message-from-main', error);
-    console.error("Failed to read file:", error);
-  }
-
-  if (!config.app.emuPath) {
-    new Notification({
-      title: 'error',
-      body: 'incorrect file path for your emulator'
-    }).show()
-  }
-
-  const setEmulatorPath = async () => {
+    let config: Config
     try {
-      await dialog.showOpenDialog({ properties: ['openFile', 'openDirectory'] }).then(res => {
+        config = getConfig()
+        console.log({ config })
+    } catch (error) {
+        mainWindow.webContents.send('message-from-main', error)
+        console.error('Failed to read file:', error)
+    }
+
+    if (!config.app.emuPath) {
+        new Notification({
+            title: 'error',
+            body: 'incorrect file path for your emulator',
+        }).show()
+    }
+
+    const setEmulatorPath = async () => {
         try {
-          // write our file path to the config.txt file
-          const filePath = path.join(filePathBase, 'config.txt');
-          mainWindow.webContents.send('message-from-main', res);
-          console.log('writing to: ', res)
-          fs.writeFileSync(filePath, `emuPath=${res.filePaths[0]}`, { encoding: 'utf8' });
+            await dialog
+                .showOpenDialog({ properties: ['openFile', 'openDirectory'] })
+                .then((res) => {
+                    try {
+                        // write our file path to the config.txt file
+                        const filePath = path.join(filePathBase, 'config.txt')
+                        mainWindow.webContents.send('message-from-main', res)
+                        console.log('writing to: ', res)
+                        fs.writeFileSync(filePath, `emuPath=${res.filePaths[0]}`, {
+                            encoding: 'utf8',
+                        })
+                    } catch (error) {
+                        mainWindow.webContents.send('message-from-main', error)
+                        console.error('Failed to write to config file:', error)
+                    }
+                })
+                .catch((err) => console.log(err))
+            // getEmulatorPath()
         } catch (error) {
-          mainWindow.webContents.send('message-from-main', error);
-          console.error("Failed to write to config file:", error);
+            console.log(error)
         }
-      }).catch(err => console.log(err))
-      // getEmulatorPath()
-    } catch (error) {
-      console.log(error)
     }
-  }
 
-  // firebase test
-  async function handleLogin(email: string, password: string) {
-    mainWindow.webContents.send('logging-in', 'trying to log in');
-    try {
-      await signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // successful sign in, send data to front end!
-          const user = userCredential.user;
-          console.log({ name: user.displayName, email: user.email, uid: user.uid })
-          mainWindow.webContents.send('login-success', { name: user.displayName, email: user.email, uid: user.uid });
+    // firebase test
+    async function handleLogin(email: string, password: string) {
+        mainWindow.webContents.send('logging-in', 'trying to log in')
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // successful sign in, send data to front end!
+                    const user = userCredential.user
+                    console.log({
+                        name: user.displayName,
+                        email: user.email,
+                        uid: user.uid,
+                    })
+                    mainWindow.webContents.send('login-success', {
+                        name: user.displayName,
+                        email: user.email,
+                        uid: user.uid,
+                    })
+                })
+                .catch((error) => {
+                    const errorCode = error.code
+                    const errorMessage = error.message
+                    console.log('failed to log in', errorCode, errorMessage)
+                    mainWindow.webContents.send('login-failed', 'login failed')
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function handleLogOut() {
+        try {
+            signOut(auth)
+                .then(() => {
+                    mainWindow.webContents.send('logged-out', 'user logged out')
+                })
+                .catch((error) => {
+                    console.log('user logging out failed', error)
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // handle ipc calls
+    ipcMain.on('login-user', (event, login) => {
+        console.log('should login user', login)
+        handleLogin(login.name, login.pass)
+    })
+
+    ipcMain.on('log-out', (event, login) => {
+        console.log('should log out user', login)
+        handleLogOut()
+    })
+
+    ipcMain.on('setEmulatorPath', () => {
+        setEmulatorPath()
+    })
+
+    // receives text from front end sends it to emulator
+    ipcMain.on('send-text', (event, text: string) => {
+        sendCommand(`textinput:${text}`)
+        // test functions
+        readCommand()
+        readStatFile(mainWindow)
+    })
+
+    ipcMain.on('send-command', (event, command) => {
+        sendCommand(command)
+        readCommand()
+        // external api testing delete me later
+        api.externalApiDoSomething(auth)
+    })
+
+    ipcMain.on('startP1', (event, data) => {
+        mainWindow.webContents.send('message-from-main', 'starting 1p mode')
+        startPlayingOnline({
+            config,
+            localPort: 7000,
+            remoteIp: data.ip || '127.0.0.1',
+            remotePort: data.port || 7001,
+            player: 0,
+            delay: 0,
         })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log('failed to log in', error.code)
-          mainWindow.webContents.send('login-failed', 'login failed');
-        });
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async function handleLogOut() {
-    try {
-      signOut(auth).then(() => {
-        console.log('user logging out')
-        mainWindow.webContents.send('logged-out', 'user logged out');
-      }).catch((error) => {
-        console.log('user logging out failed', error)
-      });
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  // handle ipc calls
-  ipcMain.on("login-user", (event, login) => {
-    console.log('should login user', login)
-    handleLogin(login.name, login.pass);
-  });
-
-  ipcMain.on("log-out", (event, login) => {
-    console.log('should log out user', login)
-    handleLogOut();
-  });
-
-  ipcMain.on("setEmulatorPath", () => {
-    setEmulatorPath();
-  });
-
-  // receives text from front end sends it to emulator
-  ipcMain.on("send-text", (event, text: string) => {
-    sendCommand(`textinput:${text}`);
-    // test functions
-    readCommand();
-    readStatFile(mainWindow);
-  });
-
-  ipcMain.on("send-command", (event, command) => {
-    sendCommand(command);
-    readCommand();
-    // external api testing delete me later
-    api.externalApiDoSomething(auth);
-    //console.log(auth)
-  });
-
-  ipcMain.on("startP1", (event, data) => {
-    mainWindow.webContents.send('message-from-main', 'starting 1p mode');
-    console.log(data)
-    startPlayingOnline({
-      config,
-      localPort: 7000,
-      remoteIp: data.ip || "127.0.0.1",
-      remotePort: data.port || 7001,
-      player: 0,
-      delay: 0,
     })
-  });
 
-  ipcMain.on("startP2", (event, data) => {
-    startPlayingOnline({
-      config,
-      localPort: 7001,
-      remoteIp: data.ip || "127.0.0.1",
-      remotePort: data.port || 7000,
-      player: 1,
-      delay: 0,
+    ipcMain.on('startP2', (event, data) => {
+        startPlayingOnline({
+            config,
+            localPort: 7001,
+            remoteIp: data.ip || '127.0.0.1',
+            remotePort: data.port || 7000,
+            player: 1,
+            delay: 0,
+        })
     })
-  });
 
-  ipcMain.on("start-solo-mode", (event) => {
-    startSoloMode({ config });
-  });
+    ipcMain.on('start-solo-mode', (event) => {
+        startSoloMode({ config })
+    })
 
-  // send a message off to websockets for other users to see and save our message on our front end.
-  ipcMain.on("sendMessage", (event, text: string) => {
-    // here we can parse the string etc
-    console.log("Main process received message:", text);
-    mainWindow.webContents.send('user-message', text);
-  });
+    // send a message off to websockets for other users to see and save our message on our front end.
+    ipcMain.on('sendMessage', (event, text: string) => {
+        // here we can parse the string etc
+        console.log('Main process received message:', text)
+        mainWindow.webContents.send('user-message', text)
+    })
 
-  // this is used by websockets to populate our chat room with other peoples messages
-  ipcMain.on("roomMessage", (event, messageObject) => {
-    // here we can parse the string etc
-    console.log(messageObject)
-    mainWindow.webContents.send('room-message', messageObject);
-  });
+    // this is used by websockets to populate our chat room with other peoples messages
+    ipcMain.on('roomMessage', (event, messageObject) => {
+        mainWindow.webContents.send('room-message', messageObject)
+    })
 
-  // add a user to the current chat room
-  ipcMain.on("addUserToRoom", (event, userObject) => {
-    console.log('adding', userObject)
-    mainWindow.webContents.send('room-users-add', userObject);
-  });
+    // add a user to the current chat room
+    ipcMain.on('addUserToRoom', (event, userObject) => {
+        mainWindow.webContents.send('room-users-add', userObject)
+    })
 
-  // remove user from the current chat room
-  ipcMain.on("removeUserFromRoom", (event, userObject) => {
-    console.log('removing', userObject)
-    mainWindow.webContents.send('room-users-remove', userObject);
-  });
+    // remove user from the current chat room
+    ipcMain.on('removeUserFromRoom', (event, userObject) => {
+        mainWindow.webContents.send('room-users-remove', userObject)
+    })
 
-  ipcMain.on("addUserGroupToRoom", (event, users) => {
-    mainWindow.webContents.send('room-users-add-group', users);
-  });
+    ipcMain.on('addUserGroupToRoom', (event, users) => {
+        mainWindow.webContents.send('room-users-add-group', users)
+    })
 
-  // and load the index.html of the app.
-  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
-  } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
-  }
+    // and load the index.html of the app.
+    if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
+    } else {
+        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`))
+    }
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+}
 
 // Listen for a request and respond to it
 ipcMain.on('request-data', (event) => {
-  event.sender.send('response-data', { msg: 'Here is some data from ipcMain' });
-});
+    event.sender.send('response-data', {
+        msg: 'Here is some data from ipcMain',
+    })
+})
 
 // read files
 setInterval(() => {
-  // currently we aren't really using this polling, but we will eventually need something like this
-  // readCommand();
-}, 1000); // read from reflector.text every 1000 ms 
+    // currently we aren't really using this polling, but we will eventually need something like this
+    // readCommand();
+}, 1000) // read from reflector.text every 1000 ms
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -241,22 +246,20 @@ app.on('ready', createWindow)
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-app.whenReady().then(() => {
-
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
+
+app.whenReady().then(() => {})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- src/preload.ts
+++ src/preload.ts
@@ -1,46 +1,51 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld("api", {
+contextBridge.exposeInMainWorld('api', {
     // server and online
-    loginUser: (loginObject: { name: string, pass: string }) => ipcRenderer.send('login-user', loginObject),
+    loginUser: (loginObject: { name: string; pass: string }) =>
+        ipcRenderer.send('login-user', loginObject),
     logOut: () => ipcRenderer.send('log-out'),
-    sendMessage: (text: string) => ipcRenderer.send("sendMessage", text),
-    sendRoomMessage: (text: string) => ipcRenderer.send("roomMessage", text),
-    addUserToRoom: (user: any) => ipcRenderer.send("addUserToRoom", user),
-    removeUserFromRoom: (user: any) => ipcRenderer.send("removeUserFromRoom", user),
-    addUserGroupToRoom: (users: [any]) => ipcRenderer.send("addUserGroupToRoom", users),
+    sendMessage: (text: string) => ipcRenderer.send('sendMessage', text),
+    sendRoomMessage: (text: string) => ipcRenderer.send('roomMessage', text),
+    addUserToRoom: (user: any) => ipcRenderer.send('addUserToRoom', user),
+    removeUserFromRoom: (user: any) => ipcRenderer.send('removeUserFromRoom', user),
+    addUserGroupToRoom: (users: [any]) => ipcRenderer.send('addUserGroupToRoom', users),
     // sends text to the emulator using the fbneo_commands.txt
     setEmulatorPath: () => ipcRenderer.send('setEmulatorPath'),
-    sendText: (text: string) => ipcRenderer.send("send-text", text),
-    sendCommand: (command: string) => ipcRenderer.send("send-command", command),
+    sendText: (text: string) => ipcRenderer.send('send-text', text),
+    sendCommand: (command: string) => ipcRenderer.send('send-command', command),
     serveMatch: (ip: string, port: number) => ipcRenderer.send('startP1', { ip, port }),
     connectMatch: (ip: string, port: number) => ipcRenderer.send('startP2', { ip, port }),
-    startSoloTraining: () => ipcRenderer.send("start-solo-mode"),
+    startSoloTraining: () => ipcRenderer.send('start-solo-mode'),
     // ipc call stuff
     on: (channel, callback) => {
-        ipcRenderer.on(channel, (event, ...args) => callback(...args));
+        ipcRenderer.on(channel, (event, ...args) => callback(...args))
     },
     removeListener: (channel, callback) => {
-        ipcRenderer.removeListener(channel, callback);
+        ipcRenderer.removeListener(channel, callback)
     },
     removeAllListeners: (channel, callback) => {
-        ipcRenderer.removeAllListeners(channel, callback);
+        ipcRenderer.removeAllListeners(channel, callback)
     },
     removeExtraListeners: (channel, callback) => {
-        console.log('we have this many listeners on ', channel, ipcRenderer.rawListeners(channel).length)
+        console.log(
+            'we have this many listeners on ',
+            channel,
+            ipcRenderer.rawListeners(channel).length
+        )
         ipcRenderer.rawListeners(channel).forEach((listener, index) => {
             console.log(listener)
             if (ipcRenderer.rawListeners(channel).length > 1) {
                 // we need to make sure we always keep alive our first index here, its the one we use in renderer.ts
                 if (index === 0) return
-                return ipcRenderer.removeListener(channel, listener);
+                return ipcRenderer.removeListener(channel, listener)
             }
             return console.log('saving last listener')
-        });
+        })
     },
-});
+})
 
 // // testing sending messages from ipc main to ipcrenderer
 // ipcRenderer.on('message-from-main', (event, message) => {
@@ -48,5 +53,5 @@ contextBridge.exposeInMainWorld("api", {
 // });
 
 ipcRenderer.on('stats-from-main', (event, message) => {
-    console.log('Stats to update the UI with:', message);
-});
+    console.log('Stats to update the UI with:', message)
+})
