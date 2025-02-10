f2aa184f3891c3ca9253faa73a5cdb73610f16b9
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-10 18:32:32 +0900

    updates answer call

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,10 +1,10 @@
-import keys from "./private/keys"
-import './index.css';
+import keys from './private/keys'
+import './index.css'
 // Load our react app
 import './front-end/app'
 
-let connectPort = 0;
-let connectIp = '0.0.0.0';
+let connectPort = 0
+let connectIp = '0.0.0.0'
 
 // document.getElementById("testBtn").addEventListener("click", () => {
 //     window.api.sendCommand("game_name");
@@ -16,9 +16,9 @@ let connectIp = '0.0.0.0';
 //     window.api.startSoloTraining();
 // });
 
-let signalServerSocket: WebSocket = null; // WebSocket reference
+let signalServerSocket: WebSocket = null // WebSocket reference
 
-let candidateList = [];
+let candidateList = []
 // handle connection to remote turn server
 
 const peerConnection = new RTCPeerConnection({
@@ -28,199 +28,209 @@ const peerConnection = new RTCPeerConnection({
         },
         {
             urls: [`turn:${keys.COTURN_IP}:${keys.COTURN_PORT}`],
-            username: "turn",
-            credential: "turn",
+            username: 'turn',
+            credential: 'turn',
         },
     ],
-});
+})
 
-let dataChannel; // Will store the game data channel
+let dataChannel // Will store the game data channel
 
 window.api.on('login-success', (user) => {
     if (user) {
-        console.log("User logged in:", user.email);
-        connectWebSocket(user);
+        console.log('User logged in:', user.email)
+        connectWebSocket(user)
     } else {
-        console.log("User not logged in. WebSocket won't connect.");
+        console.log("User not logged in. WebSocket won't connect.")
         if (signalServerSocket) {
-            signalServerSocket.close();
-            signalServerSocket = null;
+            signalServerSocket.close()
+            signalServerSocket = null
         }
     }
-});
+})
 
 window.api.on('login-failed', () => {
     // kill the socket connection
     if (signalServerSocket) {
-        signalServerSocket.close();
-        signalServerSocket = null;
+        signalServerSocket.close()
+        signalServerSocket = null
     }
-});
+})
 
 window.api.on('logged-out', (user) => {
     // kill the socket connection
     if (signalServerSocket) {
-        signalServerSocket.send(JSON.stringify({ type: "user-disconnect", user }));
-        signalServerSocket.close();
-        signalServerSocket = null;
+        signalServerSocket.send(JSON.stringify({ type: 'user-disconnect', user }))
+        signalServerSocket.close()
+        signalServerSocket = null
     }
-});
+})
 
 window.api.on('closing-app', async (user) => {
     // kill the socket connection
     if (signalServerSocket) {
         console.log('we are killing the socket user')
-        await signalServerSocket.send(JSON.stringify({ type: "user-disconnect", user }));
-        signalServerSocket.close();
-        signalServerSocket = null;
+        await signalServerSocket.send(JSON.stringify({ type: 'user-disconnect', user }))
+        signalServerSocket.close()
+        signalServerSocket = null
     }
-});
+})
 
 function connectWebSocket(user) {
-    if (signalServerSocket) return; // Prevent duplicate ws connections from same client
+    if (signalServerSocket) return // Prevent duplicate ws connections from same client
     // signalServerSocket = new WebSocket(`ws://127.0.0.1:3000`); // for testing server
-    signalServerSocket = new WebSocket(`ws://${keys.COTURN_IP}:3000`);
+    signalServerSocket = new WebSocket(`ws://${keys.COTURN_IP}:3000`)
     signalServerSocket.onopen = () => {
-        signalServerSocket.send(JSON.stringify({ type: "join", user }));
-        console.log("WebSocket connected", user.uid);
-        signalServerSocket.send(JSON.stringify({ type: "user-connect", user }));  
-    };
+        signalServerSocket.send(JSON.stringify({ type: 'join', user }))
+        console.log('WebSocket connected', user.uid)
+        signalServerSocket.send(JSON.stringify({ type: 'user-connect', user }))
+    }
 
     signalServerSocket.onclose = async (user) => {
-        if( signalServerSocket ){
-            await signalServerSocket.send(JSON.stringify({ type: "user-disconnect", user }));
+        if (signalServerSocket) {
+            await signalServerSocket.send(JSON.stringify({ type: 'user-disconnect', user }))
         }
-        console.log("WebSocket disconnected");
-        signalServerSocket = null;
-    };
+        console.log('WebSocket disconnected')
+        signalServerSocket = null
+    }
 
     signalServerSocket.onerror = (error) => {
-        console.error("WebSocket Error:", error);
-    };
+        console.error('WebSocket Error:', error)
+    }
 
     signalServerSocket.onmessage = async (message) => {
-        const data = await convertBlob(message).then(res => res);
-        if (data.type === "connected-users") {
-            if (data.users.length){
+        const data = await convertBlob(message).then((res) => res)
+        if (data.type === 'connected-users') {
+            if (data.users.length) {
                 console.log('connected users = ', data.users)
                 window.api.addUserGroupToRoom(data.users)
             }
         }
-        if (data.type === "user-connect") {
-            signalServerSocket.send(JSON.stringify({ type: "join", user }));
+        if (data.type === 'user-connect') {
+            signalServerSocket.send(JSON.stringify({ type: 'join', user }))
             // window.api.addUserToRoom(user)
         }
-        if (data.type === "user-disconnect") {
-            signalServerSocket.send(JSON.stringify({ type: "join", user }));
+        if (data.type === 'user-disconnect') {
+            signalServerSocket.send(JSON.stringify({ type: 'join', user }))
             // window.api.removeUserFromRoom(user)
         }
-        if (data.type === "user-message") {
+        if (data.type === 'user-message') {
             window.api.sendRoomMessage(data)
         }
-        if (data.type === "offer") {
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
-            const answer = await peerConnection.createAnswer();
-            await peerConnection.setLocalDescription(answer);
-            signalServerSocket.send(JSON.stringify({ type: "answer", answer }));
+        if (data.type === 'offer') {
+            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer))
+            const answer = await peerConnection.createAnswer()
+            await peerConnection.setLocalDescription(answer)
+            signalServerSocket.send(JSON.stringify({ type: 'answer', answer }))
             console.log('hey we got offer')
 
             setTimeout(async () => {
-                const stats = await peerConnection.getStats();
+                const stats = await peerConnection.getStats()
                 stats.forEach((report) => {
-                    if (report.type === "local-candidate") {
-                        console.log("Local Candidate:", report);
+                    if (report.type === 'host' || report.candidateType === 'host') return
+                    if (report.type === 'local-candidate') {
+                        console.log('Local Candidate:', report)
                     }
-                    if (report.type === "remote-candidate") {
-                        console.log("Remote Candidate:", report);
+                    if (report.type === 'remote-candidate') {
+                        console.log('Remote Candidate:', report)
                     }
-                });
-            }, 3000);
-
-        } else if (data.type === "answer") {
+                })
+            }, 3000)
+        } else if (data.type === 'answer') {
             console.log('hey we got answer')
-            console.log(JSON.stringify(candidateList));
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-
-        } else if (data.type === "ice-candidate") {
+            console.log(JSON.stringify(candidateList))
+            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer))
+        } else if (data.type === 'ice-candidate') {
             console.log('hey we got candidate')
-            await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+            await peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate))
         }
-
-    };
+    }
 
     //allow users to chat
     window.api.on('user-message', (text: string) => {
-        console.log(JSON.stringify(candidateList));
+        console.log(JSON.stringify(candidateList))
         // sends a message over to another user
         console.log('this should get sent to websockets')
-        if(text.length){
-            signalServerSocket.send(JSON.stringify({ type: 'user-message', message: `${text}`, sender: user.name }))
+        if (text.length) {
+            signalServerSocket.send(
+                JSON.stringify({ type: 'user-message', message: `${text}`, sender: user.name })
+            )
         }
-    });
-
+    })
 
     // create data channel
     function createDataChannel() {
-        dataChannel = peerConnection.createDataChannel("game", { reliable: true });
-        dataChannel.onopen = () => console.log("Data Channel Open!");
-        dataChannel.onmessage = (event) => console.log("Received:", event.data);
+        dataChannel = peerConnection.createDataChannel('game', { reliable: true })
+        dataChannel.onopen = () => console.log('Data Channel Open!')
+        dataChannel.onmessage = (event) => console.log('Received:', event.data)
     }
 
     // handle recieve data from channel
     peerConnection.ondatachannel = (event) => {
-        dataChannel = event.channel;
-        dataChannel.onopen = () => console.log("Data Channel Open!");
-        dataChannel.onmessage = (event) => console.log("Received:", event.data);
-    };
+        dataChannel = event.channel
+        dataChannel.onopen = () => console.log('Data Channel Open!')
+        dataChannel.onmessage = (event) => console.log('Received:', event.data)
+    }
 
     // send new ice candidates from the coturn server
     peerConnection.onicecandidate = (event) => {
         if (event.candidate) {
-            signalServerSocket.send(JSON.stringify({ type: "ice-candidate", candidate: event.candidate }));
-            if (event.candidate.type === "srflx" || event.candidate.type === "host") {
+            signalServerSocket.send(
+                JSON.stringify({ type: 'ice-candidate', candidate: event.candidate })
+            )
+            if (event.candidate.type === 'srflx' || event.candidate.type === 'host') {
                 // if we only require the stun server then we can break out of here.
-                console.log("STUN ICE Candidate:", event.candidate);
+                console.log('STUN ICE Candidate:', event.candidate)
                 connectPort = event.candidate.port
                 connectIp = event.candidate.address
-                candidateList.push({type: 'stun', stunAddress: event.candidate.relatedAddress, port: event.candidate.port, address: event.candidate.address})
+                candidateList.push({
+                    type: 'stun',
+                    stunAddress: event.candidate.relatedAddress,
+                    port: event.candidate.port,
+                    address: event.candidate.address,
+                })
             }
             // if the below is true it means we've successfully udp tunnelled to the candidate on the turn server
-            if (event.candidate.type === "relay") {
+            if (event.candidate.type === 'relay') {
                 // we should be able use the below information on relayed players to connect via fbneo
-                console.log("TURN ICE Candidate:", event.candidate);
+                console.log('TURN ICE Candidate:', event.candidate)
                 console.log(event.candidate.address, event.candidate.port)
                 connectPort = event.candidate.port
                 connectIp = event.candidate.address
-                console.log("UDP tunneled through TURN server!");
-                candidateList.push({type: 'turn', stunAddress: event.candidate.relatedAddress, port: event.candidate.port, address: event.candidate.address})
+                console.log('UDP tunneled through TURN server!')
+                candidateList.push({
+                    type: 'turn',
+                    stunAddress: event.candidate.relatedAddress,
+                    port: event.candidate.port,
+                    address: event.candidate.address,
+                })
             }
         }
-    };
+    }
 
     async function convertBlob(event: any) {
         try {
             // check if event is not JSON but is blob
             if (event.data instanceof Blob) {
-                const text = await event.data.text();
-                const data = JSON.parse(text);
+                const text = await event.data.text()
+                const data = JSON.parse(text)
                 console.log(data)
                 return data
             } else {
-                const data = JSON.parse(event.data);
+                const data = JSON.parse(event.data)
                 return data
             }
         } catch (error) {
-            console.error("could not convert data:", event.data, error);
+            console.error('could not convert data:', event.data, error)
         }
     }
-
 
     // Create an offer and send it to the other peer
     async function startCall() {
         createDataChannel()
-        const offer = await peerConnection.createOffer();
-        await peerConnection.setLocalDescription(offer);
-        signalServerSocket.send(JSON.stringify({ type: "offer", offer }));
+        const offer = await peerConnection.createOffer()
+        await peerConnection.setLocalDescription(offer)
+        signalServerSocket.send(JSON.stringify({ type: 'offer', offer }))
     }
 
     window.api.on('hand-shake-users', (text: string) => {
@@ -228,13 +238,13 @@ function connectWebSocket(user) {
         // if(text.length){
         //     signalServerSocket.send(JSON.stringify({ type: 'user-message', message: `${text}`, sender: user.name }))
         // }
-    });
+    })
 
     // Send Game Data
     function sendGameData(data) {
         console.log('sending data')
-        if (dataChannel && dataChannel.readyState === "open") {
-            dataChannel.send(JSON.stringify(data));
+        if (dataChannel && dataChannel.readyState === 'open') {
+            dataChannel.send(JSON.stringify(data))
         }
     }
 }
