0c9258e65248d9a8392113d0d8efccd9436aa7e2
Dustin Walker! <dustinwalkerart@gmail.com>
2025-02-28 12:25:53 +0900

    client refactor to remove async for upnp code

--- src/front-end/components/chat/UserButton.tsx
+++ src/front-end/components/chat/UserButton.tsx
@@ -1,16 +1,27 @@
+import { useLoginStore } from '../../state/store'
+
 export default function UserButton({ user }) {
+    const userState = useLoginStore((state) => state.userState)
+
     return (
         <div>
             {user.name}
-            {user.uid}
-            <button
-                onClick={() => {
-                    console.log('trying to call someone')
-                    window.api.callUser({ callerId: 'blah blah', calleeId: user.uid })
-                }}
-            >
-                challenge
-            </button>
+            {/* {user.uid} */}
+            {user.uid !== userState.uid && (
+                <button
+                    onClick={() => {
+                        console.log(
+                            'trying to call someone from: ',
+                            userState.uid,
+                            ' to => ',
+                            user.uid
+                        )
+                        window.api.callUser({ callerId: userState.uid, calleeId: user.uid })
+                    }}
+                >
+                    challenge
+                </button>
+            )}
         </div>
     )
 }
