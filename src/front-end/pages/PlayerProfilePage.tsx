f27aa1ff8a85f471135d473f461b32f51892ec50
Dustin Walker! <dustinwalkerart@gmail.com>
2025-03-17 18:23:58 +0900

    adds match cards to player profile

--- src/front-end/pages/PlayerProfilePage.tsx
+++ src/front-end/pages/PlayerProfilePage.tsx
@@ -1,17 +1,44 @@
 import { useEffect, useState } from 'react'
+import {
+    Button,
+    Stack,
+    Input,
+    Text,
+    Heading,
+    createListCollection,
+    Box,
+    Flex,
+    Center,
+    Spinner,
+    Card,
+} from '@chakra-ui/react'
+import {
+    SelectContent,
+    SelectItem,
+    SelectRoot,
+    SelectTrigger,
+    SelectValueText,
+} from '../components/chakra/ui/select'
+import { Field } from '../components/chakra/ui/field'
 import { useLoginStore } from '../state/store'
-import { Button, Stack, Input, Flex } from '@chakra-ui/react'
 
 export default function PlayerProfilePage() {
     const userState = useLoginStore((state) => state.userState)
+    const [isLoading, setIsLoading] = useState(true)
     const [recentMatches, setRecentMatches] = useState([])
 
     const handleSetRecentMatches = (matches) => {
         setRecentMatches(matches.recentMatches)
+        setIsLoading(false)
     }
 
     useEffect(() => {
+        //TODO sometimes this fails to get match data
         window.api.getUserMatches()
+        // temp
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
     }, [])
 
     useEffect(() => {
@@ -24,29 +51,96 @@ export default function PlayerProfilePage() {
         }
     }, [])
 
+    const getSuperArt = (code) => {
+        switch (parseInt(code)) {
+            case 0:
+                return 'SAI'
+                break
+            case 1:
+                return 'SAII'
+                break
+            case 2:
+                return 'SAIII'
+                break
+            default:
+                return 'SAI'
+                break
+        }
+    }
+
+    const RenderSuperArt = ({ code }) => {
+        const currentSuper = getSuperArt(code)
+        let superColor
+        if (code === 0) {
+            superColor = 'yellow.500'
+        } else if (code === 1) {
+            superColor = 'orange.500'
+        } else {
+            superColor = 'blue.500'
+        }
+        return (
+            <Text textStyle="md" padding="8px" color={superColor}>
+                {currentSuper}
+            </Text>
+        )
+    }
+
     return (
-        <Stack>
-            <Flex>Coming Soon</Flex>
-            <div>Current Username: {userState.name}</div>
-            <div>recent matches</div>
+        <Stack minH="100%">
+            {isLoading && (
+                <Box pos="absolute" inset="0" bg="bg/80">
+                    <Center h="full">
+                        <Spinner color="red.500" />
+                    </Center>
+                </Box>
+            )}
+            <Heading flex="0" size="md">
+                User Profile
+            </Heading>
             <Stack>
-                {recentMatches &&
-                    recentMatches.map((match) => {
-                        return (
-                            <Flex>
-                                {/* {match.matchId} */}
-                                <div>{match.player1Name}</div>
-                                <div>{match.player1Char}</div>
-                                <div>{match.player1Super}</div>
-                                vs
-                                <div>{match.player2Name || 'Unknown User'}</div>
-                                <div>{match.player2Char}</div>
-                                <div>{match.player1Super}</div>
-                            </Flex>
-                        )
-                    })}
-            </Stack>
-            {/* <input placeholder="User name" type="text" />
+                <div>Current Username: {userState.name}</div>
+                <div>Recent Matches</div>
+                <Stack>
+                    {recentMatches &&
+                    // TODO need to add match timestamp on BE
+                        recentMatches.sort((a, b) => a - b).map((match) => {
+                            return (
+                                    <Card.Root variant='elevated'>
+                                        <Card.Header />
+                                        <Card.Body>
+                                            {/* {match.matchId} */}
+                                            <Flex>
+                                            <Stack gap="0px">
+                                                <Text textStyle="md" padding="8px">
+                                                    {match.player1Name}
+                                                </Text>
+                                                <Text textStyle="xs" padding="8px">
+                                                    {match.player1Char}
+                                                </Text>
+                                                <RenderSuperArt code={match.player1Super} />
+                                            </Stack>
+                                            <div>
+                                                <Text textStyle="md" padding="8px">
+                                                    VS
+                                                </Text>
+                                            </div>
+                                            <Stack gap="0px">
+                                                <Text textStyle="md" padding="8px">
+                                                    {match.player2Name || 'Unknown User'}
+                                                </Text>
+                                                <Text textStyle="xs" padding="8px">
+                                                    {match.player2Char}
+                                                </Text>
+                                                <RenderSuperArt code={match.player2Super} />
+                                            </Stack>
+                                            </Flex>
+                                        </Card.Body>
+                                        <Card.Footer />
+                                    </Card.Root>
+                            )
+                        })}
+                </Stack>
+                {/* <input placeholder="User name" type="text" />
             <div> Here you can set your favorite character and see stats from matches </div>
             <div> match settings</div>
             <div> recent matches</div>
@@ -58,6 +152,7 @@ export default function PlayerProfilePage() {
                 {' '}
                 Save{' '}
             </button> */}
+            </Stack>
         </Stack>
     )
 }
