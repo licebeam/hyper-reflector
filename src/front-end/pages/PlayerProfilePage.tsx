096136b57a273ba94ac13b95decda0d9ac80b436
Dustin Walker! <dustinwalkerart@gmail.com>
2025-03-23 13:20:14 +0900

    adds pagination, skeletons and loading fixes to user profile

--- src/front-end/pages/PlayerProfilePage.tsx
+++ src/front-end/pages/PlayerProfilePage.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { useParams } from '@tanstack/react-router'
 import {
     Button,
+    IconButton,
+    ButtonGroup,
     Stack,
     Input,
     Text,
@@ -12,7 +14,10 @@ import {
     Center,
     Spinner,
     Card,
+    Pagination,
+    Skeleton,
 } from '@chakra-ui/react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import {
     SelectContent,
     SelectItem,
@@ -31,11 +36,18 @@ export default function PlayerProfilePage() {
     const [recentMatches, setRecentMatches] = useState([])
     const [userData, setUserData] = useState([])
     const [lastMatch, setLastMatch] = useState(null)
-    const [pageNumber, setPageNumber] = useState(null)
+    const [pageNumber, setPageNumber] = useState(1)
+    const [pageCount, setPageCount] = useState(null)
+    const [matchTotal, setMatchTotal] = useState(undefined)
 
     const handleSetRecentMatches = (matchData) => {
-        console.log("setting match data on front end")
-        const { matches, lastVisible } = matchData
+        const { matches, lastVisible, totalMatches } = matchData
+        // only set this once
+        if (!matchTotal) {
+            setMatchTotal(totalMatches)
+            setPageCount(Math.ceil(totalMatches / 10))
+        }
+
         setLastMatch(lastVisible)
         setRecentMatches(matches)
         setIsLoading(false)
@@ -64,12 +76,12 @@ export default function PlayerProfilePage() {
         window.api.removeExtraListeners('getUserData', handleSetUserData)
         window.api.on('getUserData', handleSetUserData)
 
-        // window.api.removeExtraListeners('getUserMatches', handleSetRecentMatches)
+        window.api.removeExtraListeners('getUserMatches', handleSetRecentMatches)
         window.api.on('getUserMatches', handleSetRecentMatches)
 
         return () => {
             window.api.removeListener('getUserData', handleSetUserData)
-            // window.api.removeListener('getUserMatches', handleSetRecentMatches)
+            window.api.removeListener('getUserMatches', handleSetRecentMatches)
         }
     }, [])
 
@@ -109,6 +121,105 @@ export default function PlayerProfilePage() {
 
     return (
         <Stack minH="100%">
+            <Heading flex="0" size="lg">
+                {userState.name}
+            </Heading>
+            {/* <Box>{userId}</Box> */}
+            <Stack>
+                <Stack>
+                    <Heading flex="0" size="md">
+                        Recent Matches
+                    </Heading>
+                    {matchTotal && (
+                        <Box>
+                            <Text textStyle="md" padding="8px">
+                                Total Matches Played: {matchTotal}
+                            </Text>
+                            <Pagination.Root
+                                count={matchTotal}
+                                pageSize={10}
+                                defaultPage={1}
+                                onPageChange={(event) => {
+                                    setIsLoading(true)
+                                    console.log(event)
+                                    if (event.page === 1) {
+                                        setLastMatch(null)
+                                    }
+                                    setPageNumber(event.page)
+                                }}
+                            >
+                                <ButtonGroup gap="4" size="sm" variant="ghost">
+                                    <Pagination.PrevTrigger asChild>
+                                        <IconButton>
+                                            <ChevronLeft />
+                                        </IconButton>
+                                    </Pagination.PrevTrigger>
+                                    <Text textStyle="md" padding="8px">
+                                        {pageNumber} of {pageCount}
+                                    </Text>
+                                    <Pagination.NextTrigger asChild>
+                                        <IconButton>
+                                            <ChevronRight />
+                                        </IconButton>
+                                    </Pagination.NextTrigger>
+                                </ButtonGroup>
+                            </Pagination.Root>
+                        </Box>
+                    )}
+
+                    {recentMatches &&
+                        recentMatches.map((match) => {
+                            return (
+                                <>
+                                    {isLoading && <Skeleton height="230px" />}
+                                    {!isLoading && (
+                                        <Card.Root variant="elevated" maxH="230px">
+                                            <Card.Header>
+                                                {new Date(
+                                                    match.timestamp._seconds * 1000
+                                                ).toLocaleString()}
+                                            </Card.Header>
+                                            <Card.Body flex="1">
+                                                <Flex>
+                                                    <Stack gap="0px" flex="1" alignItems="center">
+                                                        <Text textStyle="md" padding="8px">
+                                                            {match.player1Name}
+                                                        </Text>
+                                                        <Text textStyle="xs" padding="8px">
+                                                            {match.player1Char}
+                                                        </Text>
+                                                        <RenderSuperArt code={match.player1Super} />
+                                                    </Stack>
+                                                    <Flex flex="1" justifyContent="center">
+                                                        <Text textStyle="md" padding="8px">
+                                                            {match.results === '1' ? 1 : 0}
+                                                        </Text>
+                                                        <Text textStyle="md" padding="8px">
+                                                            VS
+                                                        </Text>
+                                                        <Text textStyle="md" padding="8px">
+                                                            {match.results === '2' ? 1 : 0}
+                                                        </Text>
+                                                    </Flex>
+                                                    <Stack gap="0px" flex="1" alignItems="center">
+                                                        <Text textStyle="md" padding="8px">
+                                                            {match.player2Name || 'Unknown User'}
+                                                        </Text>
+                                                        <Text textStyle="xs" padding="8px">
+                                                            {match.player2Char}
+                                                        </Text>
+                                                        <RenderSuperArt code={match.player2Super} />
+                                                    </Stack>
+                                                </Flex>
+                                            </Card.Body>
+                                            <Card.Footer />
+                                        </Card.Root>
+                                    )}
+                                </>
+                            )
+                        })}
+                </Stack>
+            </Stack>
             {isLoading && (
                 <Box pos="absolute" inset="0" bg="bg/80">
                     <Center h="full">
@@ -116,77 +227,6 @@ export default function PlayerProfilePage() {
                     </Center>
                 </Box>
             )}
-            <Heading flex="0" size="md">
-                User Profile
-            </Heading>
-            <div>Current Username: {userState.name}</div>
-            <Box>{userId}</Box>
-            <Stack>
-                <Heading flex="0" size="md">
-                    User Profile
-                </Heading>
-                <Button onClick={() => setPageNumber(pageNumber + 1)}> Next Page</Button>
-                <Stack>
-                    {recentMatches &&
-                        // TODO need to add match timestamp on BE
-                        recentMatches
-                            .sort((a, b) => a - b)
-                            .map((match) => {
-                                return (
-                                    <Card.Root variant="elevated">
-                                        <Card.Header />
-                                        <Card.Body flex="1">
-                                            {/* {match.matchId} */}
-                                            <Flex>
-                                                <Stack gap="0px" flex="1" alignItems="center">
-                                                    <Text textStyle="md" padding="8px">
-                                                        {match.player1Name}
-                                                    </Text>
-                                                    <Text textStyle="xs" padding="8px">
-                                                        {match.player1Char}
-                                                    </Text>
-                                                    <RenderSuperArt code={match.player1Super} />
-                                                </Stack>
-                                                <Flex flex="1" justifyContent="center">
-                                                    <Text textStyle="md" padding="8px">
-                                                        {match.results === '1' ? 1 : 0}
-                                                    </Text>
-                                                    <Text textStyle="md" padding="8px">
-                                                        VS
-                                                    </Text>
-                                                    <Text textStyle="md" padding="8px">
-                                                        {match.results === '2' ? 1 : 0}
-                                                    </Text>
-                                                </Flex>
-                                                <Stack gap="0px" flex="1" alignItems="center">
-                                                    <Text textStyle="md" padding="8px">
-                                                        {match.player2Name || 'Unknown User'}
-                                                    </Text>
-                                                    <Text textStyle="xs" padding="8px">
-                                                        {match.player2Char}
-                                                    </Text>
-                                                    <RenderSuperArt code={match.player2Super} />
-                                                </Stack>
-                                            </Flex>
-                                        </Card.Body>
-                                        <Card.Footer />
-                                    </Card.Root>
-                                )
-                            })}
-                </Stack>
-                {/* <input placeholder="User name" type="text" />
-            <div> Here you can set your favorite character and see stats from matches </div>
-            <div> match settings</div>
-            <div> recent matches</div>
-            <button
-                onClick={() => {
-                    console.log('save profile')
-                }}
-            >
-                {' '}
-                Save{' '}
-            </button> */}
-            </Stack>
         </Stack>
     )
 }
